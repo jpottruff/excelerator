fc8e6b6fd22e25a06fab7442eae235d11443b200
Jeff Pottruff <jeffpottruff@gmail.com>
2021-11-08 22:49:42 -0500

    update angular 10 to 11

--- src/app/control-panel/generate-controls/generate-controls.component.spec.ts
+++ src/app/control-panel/generate-controls/generate-controls.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GenerateControlsComponent } from './generate-controls.component';
 
@@ -6,7 +6,7 @@ describe('GenerateControlsComponent', () => {
   let component: GenerateControlsComponent;
   let fixture: ComponentFixture<GenerateControlsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GenerateControlsComponent ]
     })
